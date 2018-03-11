966ee00f2014cc7c792905f80a9daaf9879b11bc
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-03-12 01:51:46 +0530

    Spring5 - Spring Boot Joke App Example

---
+++ src/main/java/com/myprojects/spring5/examples/jokedemo/JokedemoApplication.java
@@ -0,0 +1,12 @@
+package com.myprojects.spring5.examples.jokedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokedemoApplication.class, args);
+	}
+}
