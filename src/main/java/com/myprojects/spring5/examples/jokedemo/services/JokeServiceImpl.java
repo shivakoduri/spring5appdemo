29ad89850090095863e525545a118315f3e7cfbf
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-03-18 23:23:17 +0530

    Adding Service stereotype annotation

--- src/main/java/com/myprojects/spring5/examples/jokedemo/services/JokeServiceImpl.java
+++ src/main/java/com/myprojects/spring5/examples/jokedemo/services/JokeServiceImpl.java
@@ -1,7 +1,9 @@
 package com.myprojects.spring5.examples.jokedemo.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
 
+@Service
 public class JokeServiceImpl implements JokeService{
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
